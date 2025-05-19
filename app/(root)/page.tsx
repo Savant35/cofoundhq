a9d148cd914d79dda013d501dabf92218d850527
Ali Arthur <aliarthur35@gmail.com>
2025-05-18 22:41:30 -0400

    banner implementation for homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
+import SearchForm from "@/components/SearchForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
-      <h1 className="text-2xL">HOME </h1>
+    <>
+    <section className="pink_container">
+      <h1 className="heading">
+        Pitch Your startup, <br />
+        Connect With Entreprenuerus
+      </h1>
+
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+      </p>
+      <SearchForm />
+    </section>
+    </>
+      
   );
 }
